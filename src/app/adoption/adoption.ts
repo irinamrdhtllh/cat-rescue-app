81fbefee5c8b245b8dbb70e3a2962dba4228e0d1
Irina M. S <irinamardhatillah@gmail.com>
2025-11-30 16:54:19 +0700

    Get cats data from API

--- src/app/adoption/adoption.ts
+++ src/app/adoption/adoption.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { AdoptionCard } from '../adoption-card/adoption-card';
 import { AdoptionCardInfo } from '../adoption-card-info';
 
@@ -9,50 +9,16 @@ import { AdoptionCardInfo } from '../adoption-card-info';
   styleUrl: './adoption.css',
 })
 export class Adoption {
-  adoptionCardList: AdoptionCardInfo[] = [
-    {
-      id: 0,
-      photo: '/assets/oreo.jpg',
-      name: 'Oreo',
-      city: 'Bandung',
-      state: 'West Java',
-      gender: 'Male',
-      age: '2 month',
-      breed: 'British short hair',
-      traits: 'Playful',
-    },
-    {
-      id: 1,
-      photo: '/assets/aurora.jpg',
-      name: 'Aurora',
-      city: 'Jakarta',
-      state: 'DKI Jakarta',
-      gender: 'Female',
-      age: '1 month',
-      breed: 'Ragdoll',
-      traits: 'Calm and friendly',
-    },
-    {
-      id: 2,
-      photo: '/assets/molly.jpg',
-      name: 'Molly',
-      city: 'Tangerang',
-      state: 'Banten',
-      gender: 'Female',
-      age: '3 month',
-      breed: 'Persian',
-      traits: 'Always hungry',
-    },
-    {
-      id: 3,
-      photo: '/assets/galardo.jpg',
-      name: 'Galardo',
-      city: 'Bandung',
-      state: 'West Java',
-      gender: 'Male',
-      age: '3 month',
-      breed: 'Persian',
-      traits: 'Fast and furious',
-    },
-  ];
+  catsUrl = 'http://localhost:3000/cats';
+
+  adoptionCards = signal<AdoptionCardInfo[]>([]);
+
+  async getAdoptionCardInfo(): Promise<AdoptionCardInfo[]> {
+    const cats = await fetch(this.catsUrl);
+    return (await cats.json()) ?? [];
+  }
+
+  async ngOnInit() {
+    this.adoptionCards.set(await this.getAdoptionCardInfo());
+  }
 }
