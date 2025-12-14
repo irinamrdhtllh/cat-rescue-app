2364da68569727fde4a403a5b01e3f131d69ac51
Irina M. S <irinamardhatillah@gmail.com>
2025-12-14 18:23:42 +0700

    Add pagination to adoption page

--- src/app/adoption/adoption.ts
+++ src/app/adoption/adoption.ts
@@ -11,22 +11,34 @@ import { AdoptionCardInfo } from '../adoption-card-info';
 export class Adoption {
   catsUrl = 'http://localhost:8000/cats';
 
+  currentPage = 1;
+  catsPerPage = 8;
+
+  totalItems = 0;
+  pages: number[] = [];
+
   isLoading = signal(false);
   adoptionCards = signal<AdoptionCardInfo[]>([]);
   isError = signal(false);
 
-  async getAdoptionCardInfo(city?: string): Promise<AdoptionCardInfo[]> {
+  async getAdoptionCardInfo(page: number, city?: string): Promise<AdoptionCardInfo[]> {
     try {
       this.isLoading.set(true);
-      let url;
+      let url = `${this.catsUrl}?page=${page}`;
+
       if (city) {
-        url = this.catsUrl + '?city=' + city;
-      } else {
-        url = this.catsUrl;
+        url += `&city=${city}`;
       }
-      const cats = await fetch(url);
-      const json = await cats.json();
-      return json ?? [];
+
+      const response = await fetch(url);
+      const json = await response.json();
+
+      this.totalItems = json.total;
+
+      const maxPage = Math.ceil(this.totalItems / this.catsPerPage);
+      this.pages = Array.from({ length: maxPage }, (_, i) => i + 1);
+
+      return json.data ?? [];
     } catch (error) {
       this.isError.set(true);
       return [];
@@ -35,14 +47,33 @@ export class Adoption {
     }
   }
 
+  async goToPage(page: number) {
+    this.currentPage = page;
+    this.adoptionCards.set(await this.getAdoptionCardInfo(this.currentPage));
+  }
+
+  async prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.adoptionCards.set(await this.getAdoptionCardInfo(this.currentPage));
+    }
+  }
+
+  async nextPage() {
+    if (this.currentPage < this.pages.length) {
+      this.currentPage++;
+      this.adoptionCards.set(await this.getAdoptionCardInfo(this.currentPage));
+    }
+  }
+
   async filterSubmitted(event: Event) {
     event.preventDefault();
     let formData = new FormData(event.target as HTMLFormElement);
     let city = Object.fromEntries(formData.entries())['city'];
-    this.adoptionCards.set(await this.getAdoptionCardInfo(city as string));
+    this.adoptionCards.set(await this.getAdoptionCardInfo(this.currentPage, city as string));
   }
 
   async ngOnInit() {
-    this.adoptionCards.set(await this.getAdoptionCardInfo());
+    this.adoptionCards.set(await this.getAdoptionCardInfo(this.currentPage));
   }
 }
